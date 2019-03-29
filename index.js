585268a9f2161365ac32a061dc70964ad800667c
thamizhselvi <thamizhcselvi@gmail.com>
2019-03-30 01:37:42 +0530

    updated config import path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-const config = require('config');
+const config = require('./config');
 const app = express();
 const port = 3000;
 
