f534aac057f14d3e037f231138874ad5163c0b5c
thamizhselvi <thamizhcselvi@gmail.com>
2019-03-29 23:16:23 +0530

    added mongodb and created connection

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const MongoClient = require('mongodb').MongoClient;
+
+let db;
+
+MongoClient.connect('your-mongodb-url', (err, client) => {
+    if (err) return console.log(err);
+    db = client.db('online-store');
+
+    app.listen(port, () => console.log(`Server is running listening on port ${port}!`));
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -13,5 +24,3 @@ app.get('/', (req, res) => {
 app.post('/product/create', (req, res) => {
     res.status(201).send(req.body);
 });
-
-app.listen(port, () => console.log(`Server is running listening on port ${port}!`));
